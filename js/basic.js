c950b72be60f88c8c7a04322f1eac4a13652900c
Fortune-Liu <154579349+Fortune-Liu@users.noreply.github.com>
2024-05-01 12:11:57 +0800

    Add files via upload

---
+++ js/basic.js
@@ -0,0 +1,9 @@
+md=new String();
+/*description*/
+/*only-des*/
+var only_des_button=document.createElement("only_des");
+only_des_button.setAttribute("onclick","only_des_func");
+document.getElementById("only_des").appendChild(only_des_button);
+function only_des_func(){
+    md="../resource/md/only_des.md";
+}
